68a01ccf4ec482db15a6bad70287c86e9f2a5fda
Vinicius <yamaya@ymail.com>
2021-06-29 16:40:09 -0300

    rename class

--- src/data/usecases/load-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-save-purchases.spec.ts
@@ -1,15 +1,15 @@
-import { LocalSavePurchases } from '@/data/usecases'
+import { LocalLoadPurchases } from '@/data/usecases'
 import { mockPurchases, CacheStoreSpy } from '@/data/tests'
  
 
 type SutTypes = {
-    sut: LocalSavePurchases
+    sut: LocalLoadPurchases
     cacheStore: CacheStoreSpy
 }
 
 const makeSut = (timestamp = new Date()): SutTypes => {
     const cacheStore = new CacheStoreSpy()
-    const sut =  new LocalSavePurchases(cacheStore, timestamp)
+    const sut =  new LocalLoadPurchases(cacheStore, timestamp)
     return {
         sut,
         cacheStore
